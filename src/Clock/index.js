26e3b118a27fcdcc15354e8e77b7dab7e676faef
Sebastian Romanczuk <sebastian.romanczuk1999@gmail.com>
2023-03-06 12:41:36 +0100

    add new Clock

---
+++ src/Clock/index.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import "./style.css";
+
+export const Clock = () => {
+    const [date, setDate] = useState(new Date());
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setDate(new Date());
+        }, 1000);
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, []);
+
+    return (
+        <div className="clock">
+            Dzisiaj jest
+            {" "}
+            {date.toLocaleString(undefined, {
+                weekday: "long",
+                hour: "2-digit",
+                minute: "2-digit",
+                second: "2-digit",
+                day: "numeric",
+                month: "long"
+
+            })}
+        </div>
+    )
+}
